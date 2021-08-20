d0b5d357f020d2f5f9e6783b94abfb5be74cae7b
Francisco Cerna Fukuzaki <fcernaf@gmail.com>
2021-08-20 11:48:24 -0500

    Uso de variable

--- src/actions/actionEnpoints.js
+++ src/actions/actionEnpoints.js
@@ -1,1 +1,3 @@
 export const EVALUATIONROOM_HOST = 'https://evaluationroom.herokuapp.com'
+export const EVALUATIONROOM_INICIAREXAMEN_HOST = 'https://evaluationroom-iniciarexamen.herokuapp.com'
+export const EVALUATIONROOM_SUPPORTNOTIFICATION_HOST = 'https://evaluationroom-support-notific.herokuapp.com'
