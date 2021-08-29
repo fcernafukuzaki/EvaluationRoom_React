00714b082d640ec894e5f289557b91a802586e0a
Francisco Cerna Fukuzaki <fcernaf@gmail.com>
2021-08-28 19:15:32 -0500

    3.6.0
    Eliminar archivos reducer para formulario de candidato

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,17 +1,8 @@
 import { combineReducers } from 'redux';
 
 import { actionUsuario } from './reducerUsuario';
-import { actionSexo } from './reducerSexo';
-import { actionEstadoCivil } from './reducerEstadoCivil';
-import { actionDocumentoIdentidad } from './reducerDocumentoIdentidad';
-import { actionTipoDireccion } from './reducerTipoDireccion';
-import { actionPais } from './reducerPais';
-import { actionDepartamento } from './reducerDepartamento';
-import { actionProvincia } from './reducerProvincia';
-import { actionDistrito } from './reducerDistrito';
 import { actionCliente } from './reducerCliente';
 import { actionCandidato } from './reducerCandidato';
-import { actionTestPsicologico } from './reducerTestPsicologico';
 import { actionReclutador } from './reducerReclutador';
 import { actionSelectionProcess } from './reducerSelectionProcess';
 import { actionCandidatoApreciacion } from './reducerCandidatoApreciacion';
@@ -20,22 +11,15 @@ import { actionSoporteTecnicoNotificacion } from './reducerSoporteTecnicoNotific
 import { actionCandidateResetTest } from './reducerCandidateResetTest'
 import { actionLogin } from './recruiter_view/reducerLogin'
 import { actionMenu } from './recruiter_view/reducerMenu'
+import {actionCandidateForm} from './common_view/reducerCandidateForm'
 
 const rootReducer = combineReducers({
 	reducerLogin: actionLogin,
 	reducerMenu: actionMenu,
+	reducerCandidateForm: actionCandidateForm,
 	reducerUsuario: actionUsuario,
-	reducerSexo: actionSexo,
-	reducerEstadoCivil: actionEstadoCivil,
-	reducerDocumentoIdentidad: actionDocumentoIdentidad,
-	reducerTipoDireccion: actionTipoDireccion,
-	reducerPais: actionPais,
-	reducerDepartamento: actionDepartamento,
-	reducerProvincia: actionProvincia,
-	reducerDistrito: actionDistrito,
 	reducerCliente: actionCliente,
 	reducerCandidato: actionCandidato,
-	reducerTestPsicologico: actionTestPsicologico,
 	reducerReclutador: actionReclutador,
 	reducerSelectionProcess: actionSelectionProcess,
 	reducerCandidatoApreciacion: actionCandidatoApreciacion,
